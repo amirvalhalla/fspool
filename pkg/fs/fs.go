84cbf62c0b4116ab97adbcc91d899d7df19e52f8
a.h.babazadeh <a.h.babazadeh@sgity.ir>
2023-02-28 17:09:35 +0330

    comment some codes which need to change by new structure of FileReader interface

--- pkg/fs/fs.go
+++ pkg/fs/fs.go
@@ -2,9 +2,6 @@
 package fs
 
 import (
-	"github.com/amirvalhalla/fspool/pkg/cfgs"
-	fsCfgs "github.com/amirvalhalla/fspool/pkg/cfgs/fs"
-	fsPoolCfgs "github.com/amirvalhalla/fspool/pkg/cfgs/fspool"
 	"github.com/amirvalhalla/fspool/pkg/reader"
 	"github.com/amirvalhalla/fspool/pkg/writer"
 	"sync"
@@ -29,67 +26,67 @@ type filesystem struct {
 	fileWriter writer.FileWriter
 }
 
-// Filesystem interface is top layer of FileReader and FileWriter to handle reading or writing easier
-type Filesystem interface {
-}
-
-// NewFilesystem will return a new instance from Filesystem interface
-func NewFilesystem(filePath string, fsPoolConfig fsPoolCfgs.FSPoolConfiguration, fsConfig fsCfgs.FSConfiguration) (Filesystem, error) {
-	var err error
-	var configType ConfigType
-	var config interface{}
-	var fileReader reader.FileReader
-	var fileWriter writer.FileWriter
-
-	if fsConfig.FlushSize == 0 && fsConfig.FlushDuration == 0 && fsConfig.MemoryRent == 0 && fsConfig.ReaderLimit == 0 {
-		configType = FsPoolConfigurationType
-		config = fsPoolConfig
-	} else {
-		configType = FsConfigurationType
-		config = fsConfig
-	}
-
-	if configType == FsPoolConfigurationType {
-		if fileWriter, fileReader, err = newRwBasedOnCfgs(filePath, fsPoolConfig.Perm); err != nil {
-			return nil, err
-		}
-	} else if configType == FsConfigurationType {
-		if fileWriter, fileReader, err = newRwBasedOnCfgs(filePath, fsConfig.Perm); err != nil {
-			return nil, err
-		}
-	}
-
-	return &filesystem{
-		fsConfig:   config,
-		configType: configType,
-		mu:         sync.RWMutex{},
-		fileWriter: fileWriter,
-		fileReader: fileReader,
-	}, nil
-}
-
-func newRwBasedOnCfgs(filePath string, perm cfgs.FSPerm) (writer.FileWriter, reader.FileReader, error) {
-	var err error
-	var fileReader reader.FileReader
-	var fileWriter writer.FileWriter
-
-	switch perm {
-	case cfgs.ROnly:
-		if fileReader, err = reader.NewFileReader(filePath); err != nil {
-			return nil, nil, err
-		}
-	case cfgs.WOnly:
-		if fileWriter, err = writer.NewFileWriter(filePath); err != nil {
-			return nil, nil, err
-		}
-	case cfgs.RW:
-		if fileWriter, err = writer.NewFileWriter(filePath); err != nil {
-			return nil, nil, err
-		}
-		if fileReader, err = reader.NewFileReader(filePath); err != nil {
-			return nil, nil, err
-		}
-	}
-
-	return fileWriter, fileReader, nil
-}
+//// Filesystem interface is top layer of FileReader and FileWriter to handle reading or writing easier
+//type Filesystem interface {
+//}
+//
+//// NewFilesystem will return a new instance from Filesystem interface
+//func NewFilesystem(filePath string, fsPoolConfig fsPoolCfgs.FSPoolConfiguration, fsConfig fsCfgs.FSConfiguration) (Filesystem, error) {
+//	var err error
+//	var configType ConfigType
+//	var config interface{}
+//	var fileReader reader.FileReader
+//	var fileWriter writer.FileWriter
+//
+//	if fsConfig.FlushSize == 0 && fsConfig.FlushDuration == 0 && fsConfig.MemoryRent == 0 && fsConfig.ReaderLimit == 0 {
+//		configType = FsPoolConfigurationType
+//		config = fsPoolConfig
+//	} else {
+//		configType = FsConfigurationType
+//		config = fsConfig
+//	}
+//
+//	if configType == FsPoolConfigurationType {
+//		if fileWriter, fileReader, err = newRwBasedOnCfgs(filePath, fsPoolConfig.Perm); err != nil {
+//			return nil, err
+//		}
+//	} else if configType == FsConfigurationType {
+//		if fileWriter, fileReader, err = newRwBasedOnCfgs(filePath, fsConfig.Perm); err != nil {
+//			return nil, err
+//		}
+//	}
+//
+//	return &filesystem{
+//		fsConfig:   config,
+//		configType: configType,
+//		mu:         sync.RWMutex{},
+//		fileWriter: fileWriter,
+//		fileReader: fileReader,
+//	}, nil
+//}
+//
+//func newRwBasedOnCfgs(filePath string, perm cfgs.FSPerm) (writer.FileWriter, reader.FileReader, error) {
+//	var err error
+//	var fileReader reader.FileReader
+//	var fileWriter writer.FileWriter
+//
+//	switch perm {
+//	case cfgs.ROnly:
+//		if fileReader, err = reader.NewFileReader(filePath); err != nil {
+//			return nil, nil, err
+//		}
+//	case cfgs.WOnly:
+//		if fileWriter, err = writer.NewFileWriter(filePath); err != nil {
+//			return nil, nil, err
+//		}
+//	case cfgs.RW:
+//		if fileWriter, err = writer.NewFileWriter(filePath); err != nil {
+//			return nil, nil, err
+//		}
+//		if fileReader, err = reader.NewFileReader(filePath); err != nil {
+//			return nil, nil, err
+//		}
+//	}
+//
+//	return fileWriter, fileReader, nil
+//}
--- pkg/fs/fs_test.go
+++ pkg/fs/fs_test.go
@@ -1,13 +1,7 @@
 package fs
 
-import (
-	cfgs2 "github.com/amirvalhalla/fspool/pkg/cfgs/fs"
-	cfgs "github.com/amirvalhalla/fspool/pkg/cfgs/fspool"
-	"testing"
-)
-
-func TestNewFilesystem(t *testing.T) {
-	fsPoolConfig := cfgs.FSPoolConfiguration{}
-	fsConfig := cfgs2.FSConfiguration{}
-	NewFilesystem("/record/test.txt", fsPoolConfig, fsConfig)
-}
+//func TestNewFilesystem(t *testing.T) {
+//	fsPoolConfig := cfgs.FSPoolConfiguration{}
+//	fsConfig := cfgs2.FSConfiguration{}
+//	NewFilesystem("/record/test.txt", fsPoolConfig, fsConfig)
+//}
